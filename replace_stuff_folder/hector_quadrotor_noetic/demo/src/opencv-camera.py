50985fa84577e3fb4ba476ac4ac95b0901315b90
James Murphy <jmurphyinbox@gmail.com>
2022-02-25 22:03:06 -0500

    needed to push. ubuntu updates trashed makefile

--- replace_stuff_folder/hector_quadrotor_noetic/demo/src/opencv-camera.py
+++ replace_stuff_folder/hector_quadrotor_noetic/demo/src/opencv-camera.py
@@ -3,16 +3,20 @@
 # add the line below to put_robot_in_world.launch
 # <node pkg="hector_quadrotor_demo" type="opencv-camera.py" name="start_camera" output="screen"/>
 
-
+import yolov5_detection
 import rospy
 import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from PIL import Image as PIL_img
 
 bridge = CvBridge()
 
 def img_callback(data):
     cv_image = bridge.imgmsg_to_cv2(data, "bgr8")
+    bgr2rgb = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+    real_image = PIL_img.fromarray(bgr2rgb)
+    yolov5_detection.run_detection(real_image)
     print("Camera booted!")
     cv2.imshow("Raw Image", cv_image)
     cv2.waitKey(3)
