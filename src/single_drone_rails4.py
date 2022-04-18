7b2264dd4fb404800503e60fcabc347db2a5990d
Matthew <mattbhubbs@knights.ucf.edu>
2022-04-18 17:28:15 -0400

    everything working and integrated

---
+++ src/single_drone_rails4.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python3
+
+# run this python file with either rosrun or with python3
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Int16
+isFound = False
+
+def found_callback(message):
+    isFound = True
+
+def main():
+    print("Begining rails search 4 in train_station.py")
+    rospy.init_node('single_rails_search')
+    pub = rospy.Publisher('uav4/cmd_vel', Twist, queue_size=10)
+    # this will stop the search when something is found
+    found_sub = rospy.Subscriber("found_bb8_shoot", Int16, found_callback)
+    # warm up the topic? idk?
+    for i in range(10):
+        twist = Twist()
+        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+        pub.publish(twist)
+        rospy.sleep(0.1)
+    # linear is m/s and angular is rad/s. length of large block is something like 41- -11 = 52 meters. small is 30 meters. 85 meters total
+    # block is about 85 meters wide too
+    global isFound
+    while (not isFound):
+        print("maneuver 1, up 2.5 meters in 1 second")
+        # up 2.5 meters in 1 second
+        twist = Twist()
+        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 2.5
+        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+        pub.publish(twist)
+        rospy.sleep(1) # Sleeps for 1 sec
+        print("maneuver 2, forward 10 meters in 5 seconds")
+        # forward 10 meters in 5 seconds
+        twist.linear.x = 2.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+        pub.publish(twist)
+        rospy.sleep(5)
+        print("maneuver 3, pivot turn right 90 degrees in 1 second")
+        # pivot turn right 90 degrees in 1 second
+        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = -3.8
+        pub.publish(twist)
+        rospy.sleep(1)
+        print("maneuver 4, halt 5 seconds")
+        # halt 5 seconds
+        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+        pub.publish(twist)
+        rospy.sleep(5)
+
+    # hover in place, but dont kill the process
+    twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
+    twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+    pub.publish(twist)
+    rospy.spin()
+    
+
+if __name__=="__main__":
+    main()
---
+++ src/yolov5_detection.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python3
+
+import rospy
+
+
+import rospy
+from std_msgs.msg import Bool
+import sys
+sys.path.append("./yolov5/models/")
+import torch
+import os
+from  PIL import Image
+import numpy as np
+
+def run_detection(real_image, model, cam_num):
+    # Globals    
+    SCORE_THRESHOLD = 0.5
+    print_results = False
+
+    # Verify our current Directory, Check if cuda is available for NVIDIA GPUs, else CPU (RIP AMD)
+    # print("Current Directory Scope: " + os.getcwd() + "\n")
+    # print("cuda is availible?: " + str(torch.cuda.is_available()))
+
+    # Set device to cuda or CPU, Load custom YOLOv5 model and weights, Link device to loaded model
+
+    # load an image. in the real thing, this would be a parameter
+    # Toggle a True Positive and True Negative below
+
+    # im_frame = Image.open("bb8_sample28.png") # true positive, bb8 is in this picture
+    # im_frame = Image.open("bb8_paint_true_neg.png") # true negative, bb8 is not in this image
+
+    fromPILtoNp = np.array(real_image)
+
+    # change to be tensor with axes to be (batch, channels, height, width)
+    np_frame = np.transpose(fromPILtoNp, axes=[2,0,1])
+
+    # Run the yolov5 Model {input dtype : nparray(R,G,B), output(Detections)} 
+    result = model(np_frame)
+
+    
+    # If it detected a True Positive, shows vertices for prediction, confidence score, class, name
+    # If it detected a True Negative, it will be an empty tensor instead
+    # print(result.pandas().xyxy[0])
+
+    # Isolate the confidence score
+    confidence = result.xyxyn[0][:, -2]
+    if print_results:
+        print("Confidence Score Tensor: " + str(confidence))
+
+    # If we get a True Negative, return false since we arent even looking at something that *might* be bb8
+    if len(confidence) == 0:
+        found_bb8 = False # redundant, but self documenting line
+        squint_bb8 = False
+        return found_bb8, result, squint_bb8
+
+    # This will break if we see more than one thing with a non zero confididence of it being bb8. will only take the first item
+    extracted_confidence = confidence[0]
+    if extracted_confidence >= 0.5:
+        if print_results: 
+            print("Drone " + cam_num + ": KILL HIM! KILL HIM NOW!" + ")\n")
+        found_bb8 = True
+        squint_bb8 = False
+        return found_bb8, result, squint_bb8
+    else: 
+        if print_results: 
+            print("Drone " + cam_num + ": HAHA, I'M IN DANGER " + ")\n")
+        found_bb8 = False
+        squint_bb8 = True
+        return False, result, squint_bb8
+    
+
